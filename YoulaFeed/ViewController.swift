cb6e098de56bdcd19421cde41909bbc894bb82f2
Artur Sardaryan <a.sardaryan@corp.mail.ru>
2019-03-02 18:54:02 +0300

    Initial Commit

---
+++ YoulaFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoulaFeed
+//
+//  Created by Artur Sardaryan on 3/2/19.
+//  Copyright © 2019 Artur Sardaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
